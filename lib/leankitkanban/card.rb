d41fa8df2da0406b2d4621b28a7b255462353f83
Marc Lainez <marc.lainez@gmail.com>
2011-10-29 10:08:08 +0200

    Add card find by external id api call

--- lib/leankitkanban/card.rb
+++ lib/leankitkanban/card.rb
@@ -3,10 +3,16 @@ module LeanKitKanban
     include HTTParty
     include LeanKitRequest
 
-    FIND_CARD = "/Board/{boardID}/GetCard/{cardID}"
+    FIND_CARD     = "/Board/{boardID}/GetCard/{cardID}"
+    FIND_CARD_EXT = "/Board/{boardID}/GetCardByExternalId/{externalID}"
 
     def self.find(board_id, card_id)
       api_call = FIND_CARD.gsub("{boardID}", board_id.to_s).gsub("{cardID}", card_id.to_s)
+      get(api_call)
+    end
+
+    def self.find_by_external_id(board_id, external_id)
+      api_call = FIND_CARD_EXT.gsub("{boardID}", board_id.to_s).gsub("{externalID}", external_id.to_s)
       get(api_call)
     end
   end
--- spec/leankitkanban/card_spec.rb
+++ spec/leankitkanban/card_spec.rb
@@ -13,4 +13,17 @@ describe LeanKitKanban::Card do
       LeanKitKanban::Card.find(@board_id, @card_id)
     end
   end
+
+  describe :find_by_external_id do
+    before :each do
+      @board_id    = mock("boardID")
+      @external_id = mock("externalID")
+    end
+
+    it "gets the board card whose id is passed" do
+      api_call = "/Board/#{@board_id}/GetCardByExternalId/#{@external_id}"
+      LeanKitKanban::Card.should_receive(:get).with(api_call)
+      LeanKitKanban::Card.find_by_external_id(@board_id, @external_id)
+    end
+  end
 end
