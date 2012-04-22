9b3c9436b0791b04207a7d0323951618e6e01be8
Darren Cauthon <darren@cauthon.com>
2012-04-22 14:44:03 -0500

    Refactor new get_single_result to leankitkanban module.

--- lib/leankitkanban/card.rb
+++ lib/leankitkanban/card.rb
@@ -9,21 +9,13 @@ module LeanKitKanban
     def self.find(board_id, card_id)
       api_call = FIND_CARD.gsub("{boardID}", board_id.to_s).gsub("{cardID}", card_id.to_s)
       result = get(api_call)
-      get_single_result(result)
+      LeanKitKanban.get_single_result(result)
     end
 
     def self.find_by_external_id(board_id, external_id)
       api_call = FIND_CARD_EXT.gsub("{boardID}", board_id.to_s).gsub("{externalID}", external_id.to_s)
       result = get(api_call)
-      get_single_result(result)
-    end
-
-    private
-
-    def self.get_single_result(results)
-      return results if results.nil?
-      return results[0] if results.kind_of?(Array)
-      results
+      LeanKitKanban.get_single_result(result)
     end
   end
 end
